aa3dd5c9392f32adc13e612bab6d688cf41bf932
gagikm@ucla.edu <gagikm@ucla.edu>
2018-04-24 21:28:49 -0700

    Cache search queries

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,23 @@
 class LocationsController < ApplicationController
   def index
-    @locations = MetaWeather.new(params).search if params[:query]
+    search_query = params[:query]
+
+    if !search_query
+      return
+    end
+
+    search_query = search_query.downcase
+    existing_entry = CacheEntry.find_by(query: search_query)
+
+    if existing_entry
+      @locations = JSON.parse(existing_entry.response)
+    else
+      @locations = MetaWeather.new(params).search if search_query
+      entry = CacheEntry.new
+      entry.query = search_query
+      entry.response = @locations.to_json
+      entry.save
+    end
   end
 
   def show
