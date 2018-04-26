9c20faa17b8cca76184c57e06fbb16cc20bda364
gagikm@ucla.edu <gagikm@ucla.edu>
2018-04-25 20:05:18 -0700

    Add routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root controller: :locations, action: :index
 
   resources :locations, only: %i[index show]
+  resources :admin, only: %i[index create destroy]
 end
