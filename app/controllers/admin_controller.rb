5938c095b35e4b65dec566b49119b555e90f3ef9
Gagik Movsisyan <gagikm@ucla.edu>
2018-04-25 20:36:46 -0700

    Fix indents

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,19 +1,17 @@
 class AdminController < ApplicationController
   def index
-  	@cached_entries = CacheEntry.all.order('id desc')
+    @cached_entries = CacheEntry.all.order('id desc')
   end
 
   def destroy
-  	puts params[:id]
-  	puts "HELLO"
     CacheEntry.destroy(params[:id])
     redirect_to request.referrer
   end
 
   def create
-  	query_to_cache = params[:query]
+    query_to_cache = params[:query]
 
-  	if query_to_cache and !CacheEntry.find_by(query: query_to_cache)
+    if query_to_cache and !CacheEntry.find_by(query: query_to_cache)
       @locations = MetaWeather.new(params).search if query_to_cache
       entry = CacheEntry.new
       entry.query = query_to_cache
